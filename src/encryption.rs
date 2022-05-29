68e112c3952987562ba0360cc3a5c813f7f287bb
Paulo Albuquerque <paulora2405@gmail.com>
2022-05-29 11:24:49 -0300

    Add WIP text to numeric conversion

--- src/encryption.rs
+++ src/encryption.rs
@@ -1,34 +1,30 @@
 use num_bigint::BigUint;
-use num_traits::Zero;
-
-pub fn log_b(num: &BigUint) -> u16 {
-    let mut num_ = num.clone();
-    let mut bits: u16 = 0;
-    while !num_.is_zero() {
-        num_ >>= 1u8;
-        bits += 1;
-    }
-    bits
-}
 
 pub fn text_to_numeric(text: &String) -> BigUint {
-    let mut num = BigUint::from(0u8);
-    let mut e = 0u32;
-    for c in text.chars() {
-        // mult by 32 because a char uses 4 bytes in rust
-        num += u32::from(c) * BigUint::from(1u8) << (e * 8);
-        e += 1;
-    }
-    num
+    BigUint::from_bytes_le(text.as_bytes())
 }
 
-// pub fn numeric_to_text(num: &BigUint) -> String {
-//     let mut num_ = num.clone();
-//     let mut text = String::from("");
-//     while !num.is_zero() {
-//         // let x: u8 = *&num_ & 255u8;
-//         // let ch: char = char::from(x);
-//         // text.push(ch);
-//     }
-//     text
-// }
+pub fn numeric_to_text(num: &BigUint) -> String {
+    String::from_utf8(num.to_bytes_le()).expect("Could not convert Numeric value to a String")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_numeric_to_text() {
+        let bytes: Vec<u8> = vec![84u8, 101u8, 115u8, 116u8];
+        let num = BigUint::from_bytes_le(&bytes);
+        println!("{}", num.bits());
+        let text = numeric_to_text(&num);
+        assert_eq!(text, "Test");
+    }
+
+    #[test]
+    fn test_text_to_numeric() {
+        let text = "Test";
+        let num = text_to_numeric(&text.to_owned());
+        println!("{}", num);
+    }
+}
