ca93caed7b13de3c2f5ad8d6d8f768be02232158
Paulo Albuquerque <paulora2405@gmail.com>
2023-03-17 16:58:00 -0300

    Optimize cypher file size

--- src/encryption.rs
+++ src/encryption.rs
@@ -5,13 +5,15 @@ use std::fs::{create_dir_all, File};
 use std::io::{Read, Write};
 use std::path::{Path, MAIN_SEPARATOR};
 
+const ENCRYPTION_BYTE_OFFSET: usize = 1;
+
 /// Returns the number of bytes needed to store all the bits of N-1
 fn size_in_bytes(n: &BigUint) -> usize {
     let one = &BigUint::from(1u32);
 
     assert!(n > one);
 
-    let nb_bits = (n - one).bits();
+    let nb_bits = n.bits();
     let nb_bytes = nb_bits / 8;
 
     nb_bytes
@@ -49,57 +51,44 @@ fn open_input_output(file_path: &str, out_path: &str) -> (File, File) {
 
 /// Encrypts a file chunk by chunk
 pub fn encrypt_file(file_path: &str, out_path: &str, key: &Key) {
-    // println!("----------------------------------------------");
-    // println!("----------------------------------------------");
-    // println!("Encrypting...");
-    // println!("----------------------------------------------");
-    // println!("----------------------------------------------");
     let (mut file_in, mut file_out) = open_input_output(file_path, out_path);
 
     let (exponent, modulus) = (&key.d_e, &key.n);
-    // dbg!(&exponent);
-    // dbg!(&modulus);
+    // dbg!(&exponent, &modulus);
 
-    // dbg!(size_in_bytes(modulus));
-    let max_bytes_read: usize = size_in_bytes(modulus) - 1; // MINUS ONE
-    let max_bytes_write: usize = size_in_bytes(modulus) + 1; // MINUS ONE
-                                                             // dbg!(max_bytes);
+    let max_bytes_read: usize = size_in_bytes(modulus) - ENCRYPTION_BYTE_OFFSET;
+    let max_bytes_write: usize = size_in_bytes(modulus);
+    // dbg!(size_in_bytes(modulus), max_bytes_read, max_bytes_write);
+    assert!(max_bytes_read > 0); // never will be > 0 because min key size is 32 bits == 4 bytes
+
     let mut source_bytes = vec![0u8; max_bytes_read];
     let mut destiny_bytes = Vec::<u8>::with_capacity(max_bytes_read);
     let mut bytes_amount_read = max_bytes_read;
 
     // let mut i = 1;
     while bytes_amount_read == max_bytes_read {
-        // println!("--------\nIter {i}");
-        // i += 1;
-
         source_bytes.fill(0u8);
         bytes_amount_read = file_in.read(&mut source_bytes).unwrap();
-        // dbg!(bytes_amount_read);
-
         if bytes_amount_read == 0 {
             break;
         }
 
+        // println!("--------\nIter {i}");
+        // i += 1;
+        // dbg!(bytes_amount_read);
         // dbg!(String::from_utf8_lossy(&source_bytes));
 
         let message = BigUint::from_bytes_le(&source_bytes);
         let encrypted = mod_pow(&message, exponent, modulus);
-        // dbg!(&message);
-        // dbg_hex!(&message.to_bytes_le());
-        // dbg!(&encrypted);
-        // dbg_hex!(&encrypted.to_bytes_le());
+        // dbg_hex::dbg_hex!(&message.to_bytes_le(), &encrypted.to_bytes_le());
 
         destiny_bytes.clear();
         let _ = destiny_bytes.write(&encrypted.to_bytes_le()).unwrap();
 
-        // dbg_hex!(&destiny_bytes);
         if destiny_bytes.len() < max_bytes_write {
             let size_diff = (max_bytes_write) - destiny_bytes.len();
-            // dbg!(size_diff);
             destiny_bytes.append(&mut vec![0u8; size_diff]);
         }
-        // dbg_hex!(&destiny_bytes);
 
         let _bytes_amount_written = file_out.write(&destiny_bytes).unwrap();
         // dbg!(_bytes_amount_written);
@@ -108,51 +97,40 @@ pub fn encrypt_file(file_path: &str, out_path: &str, key: &Key) {
 
 /// decrypts a file chunk by chunk
 pub fn decrypt_file(file_path: &str, out_path: &str, key: &Key) {
-    // println!("----------------------------------------------");
-    // println!("----------------------------------------------");
-    // println!("Decrypting...");
-    // println!("----------------------------------------------");
-    // println!("----------------------------------------------");
     let (mut file_in, mut file_out) = open_input_output(file_path, out_path);
 
     let (exponent, modulus) = (&key.d_e, &key.n);
-    // dbg!(&exponent);
-    // dbg!(&modulus);
+    // dbg!(&exponent, &modulus);
 
-    // dbg!(size_in_bytes(modulus));
-    let max_bytes = size_in_bytes(modulus) + 1; // PLUS ONE
-                                                // dbg!(max_bytes);
+    let max_bytes = size_in_bytes(modulus);
+    // dbg!(size_in_bytes(modulus), max_bytes);
+
     let mut source_bytes = vec![0u8; max_bytes];
     let mut destiny_bytes = Vec::<u8>::with_capacity(max_bytes);
     let mut bytes_amount_read = max_bytes;
 
     // let mut i = 1;
     while bytes_amount_read == max_bytes {
-        // println!("--------\nIter {i}");
-        // i += 1;
-
-        source_bytes.fill(0u8);
+        source_bytes.fill(0u8); // source
         bytes_amount_read = file_in.read(&mut source_bytes).unwrap();
-        // dbg!(bytes_amount_read);
-
         if bytes_amount_read == 0 {
             break;
         }
 
+        // println!("--------\nIter {i}");
+        // i += 1;
+        // dbg!(bytes_amount_read);
+
         let encrypted = BigUint::from_bytes_le(&source_bytes);
         let message = mod_pow(&encrypted, exponent, modulus);
-        // dbg!(&encrypted);
-        // dbg_hex!(&encrypted.to_bytes_le());
-        // dbg!(&message);
-        // dbg_hex!(&message.to_bytes_le());
+        // dbg_hex::dbg_hex!(&encrypted, &message);
+        // dbg_hex::dbg_hex!(&encrypted.to_bytes_le(), &message.to_bytes_le());
 
         destiny_bytes.clear();
         let _ = destiny_bytes.write(&message.to_bytes_le()).unwrap();
-        // dbg_hex!(&destiny_bytes);
 
         let _bytes_amount_written = file_out.write(&destiny_bytes).unwrap();
         // dbg!(_bytes_amount_written);
-
         // dbg!(String::from_utf8_lossy(&destiny_bytes));
     }
 }
