051a7273f99b251c0c1dcdba810fc6beafa648df
Paulo Albuquerque <paulora2405@gmail.com>
2025-12-19 11:28:48 -0300

    add some functions

--- benches/buffer_read_bench.rs
+++ benches/buffer_read_bench.rs
@@ -1,6 +1,7 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 use rrsa_lib::key::{Key, KeyPair};
 use std::fs::File;
+use std::io::{BufRead, Read};
 use std::str::FromStr;
 
 fn bufreader_bench(c: &mut Criterion) {
@@ -80,4 +81,64 @@ fn pair_4096() -> KeyPair {
         public_key,
         private_key,
     }
+}
+
+const FILENAME: &str = "messages/big.txt";
+
+pub fn read_unbuffered_one_character_at_a_time() -> std::io::Result<u64> {
+    let mut file = File::open(FILENAME)?;
+    let len = file.metadata().expect("Failed to get file metadata").len() as usize;
+    let mut v: Vec<u8> = vec![0u8; len];
+    for index in 0..len {
+        file.read_exact(&mut v[index..(index + 1)])?;
+    }
+    let s = String::from_utf8(v).expect("file is not UTF-8?");
+    let mut total = 0u64;
+    for line in s.lines() {
+        total += get_count_from_line(line);
+    }
+    Ok(total)
+}
+
+pub fn read_buffered_allocate_string_every_time() -> std::io::Result<u64> {
+    let file = File::open(FILENAME)?;
+    let reader = std::io::BufReader::new(file);
+    let mut total = 0u64;
+    for line in reader.lines() {
+        let s = line?;
+        total += get_count_from_line(&s);
+    }
+    Ok(total)
+}
+
+pub fn read_buffered_reuse_string() -> std::io::Result<u64> {
+    let file = File::open(FILENAME)?;
+    let mut reader = std::io::BufReader::new(file);
+    let mut string = String::new();
+    let mut total = 0u64;
+    while reader.read_line(&mut string).unwrap() > 0 {
+        total += get_count_from_line(&string);
+        string.clear();
+    }
+    Ok(total)
+}
+
+pub fn read_buffer_whole_string_into_memory() -> std::io::Result<u64> {
+    let mut file = File::open(FILENAME)?;
+    let mut s = String::new();
+    file.read_to_string(&mut s)?;
+    let mut total = 0u64;
+    for line in s.lines() {
+        total += get_count_from_line(line);
+    }
+    Ok(total)
+}
+
+fn get_count_from_line(s: &str) -> u64 {
+    if s.is_empty() {
+        return 0;
+    }
+    let mut parts = s.split_ascii_whitespace();
+    let _ = parts.next();
+    parts.next().unwrap().parse::<u64>().unwrap()
 }
